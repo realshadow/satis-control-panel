cac0782dc156584acc4b125a569544a8508da7c7
neilime <neilime@users.noreply.github.com>
2016-03-26 14:48:48 +0100

    Supports https host

--- node/server.js
+++ node/server.js
@@ -1,12 +1,25 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var jf = require('jsonfile');
 var fs = require('fs');
+var jf = require('jsonfile');
 var config = jf.readFileSync(__dirname + '/config.json');
-
 var lockFile = __dirname + '/../' + config['lock_file'];
 
+// Initialize server
+if(/^https/.test(config.host)){
+    var server = require('https').Server({
+      key: fs.readFileSync(config.SSLCertificateKeyFile),
+      cert: fs.readFileSync(config.SSLCertificateFile)
+    }, app);
+}
+else{
+     var server = require('http').Server(app);
+}
+server.listen(config.port, function() {
+    console.log('Listening on *:' + config.port);
+});
+
+// Initialize socket
+var io = require('socket.io')(server);
 io.sockets.on('connection', function(socket) {
 	fs.watchFile(lockFile, function(event, fileName) {
 		jf.readFile(lockFile, function(err, data) {
@@ -15,10 +28,7 @@ io.sockets.on('connection', function(socket) {
 	});
 });
 
+// Initialize routing
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
-});
-
-http.listen(config.port, function() {
-	console.log('Listening on *:' + config.port);
 });
