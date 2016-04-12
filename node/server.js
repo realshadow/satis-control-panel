1c91245fcb4e306dd0c06147ac2c2b6ca2d26369
Lukas Homza <lukashomz@gmail.com>
2016-04-12 08:04:38 +0200

    verbose error when no ssl certificates are provided

--- node/server.js
+++ node/server.js
@@ -6,7 +6,11 @@ var lockFile = __dirname + '/../' + config['lock_file'];
 
 // Initialize server
 var server;
-if(/^https/.test(config.host)) {
+if (/^https/.test(config.host)) {
+    if (config.SSLCertificateKeyFile === '' || config.SSLCertificateFile === '') {
+        throw 'If you want to use HTTPS server, path to SSL certificates must be provided.';
+    }
+
     server = require('https').Server({
       key: fs.readFileSync(config.SSLCertificateKeyFile),
       cert: fs.readFileSync(config.SSLCertificateFile)
